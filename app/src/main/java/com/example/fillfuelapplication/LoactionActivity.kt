089ae49c8fad85e873165b0967e4adfd8ab28cf1
CHEHARA <pererachehara@gmail.com>
2023-04-19 20:19:07 +0530

    Test 1

---
+++ app/src/main/java/com/example/fillfuelapplication/LoactionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fillfuelapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoactionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loaction)
+    }
+}
